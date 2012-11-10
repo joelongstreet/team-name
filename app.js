45369b32b5f38f865de0975b570cd0da85973912
Russell Madsen <ruzz311@gmail.com>
2012-11-09 23:08:25 -0600

    add reddis server and organize

--- app.js
+++ app.js
@@ -1,51 +1,62 @@
-// My SocketStream 0.3 app
+// RowBro!
 
 var http = require('http'),
     ss = require('socketstream');
 
-// Define a single-page client called 'main'
-ss.client.define('main', {
-  view: 'app.jade',
-  css:  ['libs/reset.css', 'app.styl'],
-  code: ['libs/jquery.min.js', 'app'],
-  tmpl: '*'
-});
 
-// Define a single-page client called 'remote'
-ss.client.define('remote', {
-  view: 'remote.jade',
-  css:  ['libs/reset.css', 'app.styl'],
-  code: ['libs/jquery.min.js', 'app'],
-  tmpl: ['common', 'remote']
-});
+//============================================================ Define SS clients
 
-// Serve this client on the root URL
-ss.http.route('/', function(req, res){
-  ua = req.headers['user-agent']
-  if ( /mobile/i.test(ua) )
-    res.serveClient('remote')
-  else
-    res.serveClient('main');
-});
+  // Define a single-page client called 'main'
+  ss.client.define('main', {
+    view: 'app.jade',
+    css:  ['libs/reset.css', 'app.styl'],
+    code: ['libs/jquery.min.js', 'app'],
+    tmpl: '*'
+  });
 
-// Code Formatters
-ss.client.formatters.add(require('ss-coffee'));
-ss.client.formatters.add(require('ss-stylus'));
-ss.client.formatters.add(require('ss-jade'));
+  // Define a single-page client called 'remote'
+  ss.client.define('remote', {
+    view: 'remote.jade',
+    css:  ['libs/reset.css', 'app.styl'],
+    code: ['libs/jquery.min.js', 'app'],
+    tmpl: ['common', 'remote']
+  });
 
-// Use server-side compiled Hogan (Mustache) templates. Others engines available
-ss.client.templateEngine.use(require('ss-hogan'));
 
-// Minimize and pack assets if you type: SS_ENV=production node app.js
-if (ss.env === 'production') ss.client.packAssets();
+//================================================================== HTTP Router
 
-// Start web server
-var server = http.Server(ss.http.middleware);
-server.listen(3000);
+  // Serve this client on the root URL
+  ss.http.route('/', function(req, res){
+    ua = req.headers['user-agent']
+    client = /mobile/i.test(ua) ? 'remote' : 'main'
+    res.serveClient(client);
+  });
 
-// Start ss-console
-var ssconsole = require('ss-console')(ss);
-ssconsole.listen(5000);
 
-// Start SocketStream
-ss.start(server);
+//================================================================ Server Config
+  
+  // the in-built Redis transport
+  // any config can be passed to the second argument
+  ss.publish.transport.use('redis', {}); 
+
+  // Code Formatters
+  ss.client.formatters.add(require('ss-coffee'));
+  ss.client.formatters.add(require('ss-stylus'));
+  ss.client.formatters.add(require('ss-jade'));
+
+  // Use server-side compiled Hogan (Mustache) templates.
+  ss.client.templateEngine.use(require('ss-hogan'));
+
+  // Minimize and pack assets if you type: SS_ENV=production node app.js
+  if (ss.env === 'production') ss.client.packAssets();
+
+  // Start web server
+  var server = http.Server(ss.http.middleware);
+  server.listen(3000);
+
+  // Start ss-console
+  var ssconsole = require('ss-console')(ss);
+  ssconsole.listen(5000);
+
+  // Start SocketStream
+  ss.start(server);
